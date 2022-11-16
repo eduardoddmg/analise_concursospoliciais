e881c932acee7805f87a8c53c7a7efe462979839
Eduardo Melo <92065918+eduardoddmg@users.noreply.github.com>
2022-11-16 16:34:57 -0300

    update code

--- main.py
+++ main.py
@@ -1,101 +1,69 @@
-import pandas as pd
-from func import *
-import numpy as np
+import pandas
 
-def printar(lista):
-	for i in lista:
-		print(i)
+dictionary = {
+	"aprovados": "cod,nome,notas p1,acertos p1,notas p2,acertos p2,nf\n",
+	"rest": "cod, nome\n"
+}
 
-def read(file):
-	f = open(file, 'r')
-	return format(f.read())
+# formatar txt
+def prepare_file(path, final, cols):
+	f = open(path)
+	text = f.read()
+	f.close()
 
-def format(array):
-	array = array.replace('\n', ' ')
-	array = array.split(' /')
-	for i in range(0, len(array)):
-		array[i] = array[i].split(', ')
-		array[i][0] = array[i][0].replace(' ', '')
-	return array
+	text = text.replace('\n', ' ').replace(', ', ',').replace(' / ', '\n').replace('\n ', '\n')
+	text = cols+text
 
-def reFormart(aprovados):
-	cod = parseSeparate(aprovados, 0, False)
-	nome = parseSeparate(aprovados, 1, False)
-	nota_p1 = parseSeparate(aprovados, 2, True)
-	acertos_p1 = parseSeparate(aprovados, 3, True)
-	nota_p2 = parseSeparate(aprovados, 4, True)
-	acertos_p2 = parseSeparate(aprovados, 5, True)
-	nota_final = parseSeparate(aprovados, 6, True)
-	return [cod, nome, nota_p1, acertos_p1, nota_p2, acertos_p2, nota_final]
+	f = open(final, 'w')
+	f.write(text)
 
-def parseSeparate(array, index, number):
-	new_arr = []
-	for i in array:
-		if (i[index].find(' ') == 0):
-			i[index] = i[index].replace(' ', '', 1)
-		if (i[index].find(' ') == len(i[index])-1):
-			i[index] = i[index][:-1]
-		if (not number):
-			new_arr.append(i[index])
+def classificacao(aprovados):
+	result = []
+	for row in aprovados.index:
+		if aprovados['taf'][row] == 'SIM' and aprovados['medico'][row] == 'SIM' and aprovados['odonto'][row] == 'SIM' and aprovados['psico'][row] == 'SIM' and aprovados['is'][row] == 'SIM':
+			result.append('SIM')
 		else:
-			new_arr.append(float(i[index]))
-	return new_arr
+			result.append('NÃO')
+	aprovados["classificado"] = result
+	return aprovados
 
-def filter(arr, string):
-	arr = np.array(arr)
-	filter_arr = arr == string
-	return arr[filter_arr]
+def addColumn(aprovados, fase, name):
+	result = []
+	cod_fase = fase["cod"].values
+	cod_aprovados = aprovados["cod"].values
+	for i in cod_aprovados:
+		if i in cod_fase:
+			result.append("SIM")
+		else: 
+			result.append("NÃO")
+	aprovados[name] = result
+	aprovados.to_csv('./csv/aprovados.csv')
+	return aprovados
+	
 
-def changeBool(arr):
-	for i in range(0, len(arr)):
-		if arr[i] == True:
-			arr[i] = "APTO"
-		else:
-			arr[i] = "INAPTO"
-	return arr
+def generate_csv(name, cols):
+	path = './files/'+name+'.txt'
+	final = './files_tratados/'+name+'.txt'
+	csv = './csv/'+name+'.csv'
+	prepare_file(path, final, cols)
+	df = pandas.read_csv(final)
+	df.to_csv(csv)
+	return pandas.read_csv(final)
 
-def init():
-	print("hello world")
+aprovados = generate_csv('aprovados', dictionary["aprovados"])
+taf = generate_csv('taf', dictionary["rest"])
+medico = generate_csv('medico', dictionary["rest"])
+odonto = generate_csv('odonto', dictionary["rest"])
+psico = generate_csv('psico', dictionary["rest"])
+invest_social = generate_csv('is', dictionary["rest"])
 
-	aprovados = read('aprovados.txt')
-	taf_aprovados = read('./files/taf.txt')
-	medico_aprovados = read('./files/medico.txt')
-	odonto_aprovados = read('./files/odonto.txt')
-	psico_aprovados = read('./files/psico.txt')
-	invest_social_aprovados = read('./files/is.txt')
+aprovados = addColumn(aprovados, taf, 'taf')
+aprovados = addColumn(aprovados, medico, 'medico')
+aprovados = addColumn(aprovados, odonto, 'odonto')
+aprovados = addColumn(aprovados, psico, 'psico')
+aprovados = addColumn(aprovados, invest_social, 'is')
 
-	cod_aprovados = parseSeparate(aprovados, 0, False)
-	cod_taf = parseSeparate(taf_aprovados, 0, False)
-	cod_medico = parseSeparate(medico_aprovados, 0, False)
-	cod_odonto = parseSeparate(odonto_aprovados, 0, False)
-	cod_psico = parseSeparate(psico_aprovados, 0, False)
-	cod_invest_social = parseSeparate(invest_social_aprovados, 0, False)
-
-	aprovadosToPlot = reFormart(aprovados)
-
-	taf = codUnion(cod_aprovados, cod_taf)
-	medico = codUnion(cod_aprovados, cod_medico)
-	odonto = codUnion(cod_aprovados, cod_odonto)
-	psico = codUnion(cod_aprovados, cod_psico)
-	invest_social = codUnion(cod_aprovados, cod_invest_social)
-	tox = [False]*len(aprovados)
-
-	classificados = classificadosId(aprovados, [taf, medico, odonto, invest_social, psico])
-
-	taf = changeBool(taf)
-	medico = changeBool(medico)
-	odonto = changeBool(odonto)
-	psico = changeBool(psico)
-	invest_social = changeBool(invest_social)
-	tox = changeBool(tox)
-
-	classificados_aptos = filter(classificados, "YES")
-	classificados_inaptos = filter(classificados, "NO")
-
-	qtd_aprovados = [len(cod_aprovados), len(cod_taf), len(cod_medico), len(cod_odonto), len(cod_psico), len(cod_invest_social)]
-
-	# ## setup pandas
-	plot(aprovadosToPlot, [taf, medico, odonto, invest_social, psico, tox], classificados)
-	plot_graphic(qtd_aprovados)
-	plot_bar([len(classificados_aptos), len(classificados_inaptos)])
-init()
+aprovados = classificacao(aprovados)
+aprovados = aprovados.sort_values(by=['classificado', 'nf','nf', 'notas p2', 'acertos p2', 'acertos p1'], ascending=False)
+print(aprovados)
+aprovados.to_csv('aprovados.csv')
